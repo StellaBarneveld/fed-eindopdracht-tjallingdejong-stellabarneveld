5a87de193a66f2aec8931b1eaf7d00f7715c2bad
StellaBarneveld <s.a.m.barneveld@st.hanze.nl>
2023-03-16 11:09:45 +0100

    Set-up

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Motivational Quote Generator
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        Tjalling de Jong en Stella Barneveld
+        </p>
       </header>
     </div>
   );
