b27d3a1af134e2eff2381c7913cd382f9ada5250
ASNdommegang <tjallingvanassen@hotmail.com>
2023-03-20 15:53:48 +0100

    Homepage
    Starsigns erin gezet + de background aangepast + standaard tekst weg gehaald

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
+import starsign from './starsign-circle.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Motivational Quote Generator
-        </p>
-        <p>
-        Tjalling de Jong en Stella Barneveld
-        </p>
+        <img src= {starsign} className="App-logo" alt="Starsigns" />
+        <a href=''>
+          <h1>Generate a new quote</h1>
+          </a>      
       </header>
     </div>
   );
