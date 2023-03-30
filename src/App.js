3b98e7a73eec486c9d6253c46bba84fdf7b4f7b2
StellaBarneveld <s.a.m.barneveld@st.hanze.nl>
2023-03-30 11:33:48 +0200

    trying to make it work
    #doesn't

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import starsign from './starsign_gradient.svg';
 import vrouw from './waarzegger.png'
 import './App.css';
 import RandomQuote from './quote';
 
+export default function App (){
+  const [quote, setQuote] = useState();
+const [img, setImg] = useState();
+const generateQuote = (e) => {
 
-  
+  e.preventDefault();
 
-function App() {
+  fetch('https://type.fit/api/quotes')
+  .then(results => results.json())
+  .then(data => {
+      console.log(data)
+      let randomNumber = Math.round(Math.random() * (data.length - 0) + 0);
+      setQuote(data[randomNumber].text)
+
+  });
+
+  fetch('image.json')
+  .then(results => results.json())
+  .then(data => {
+      console.log(data.images)
+      let randomNumber = Math.round(Math.random() * (data.images.length - 0) + 0);
+      setImg(data.images[randomNumber])
+      console.log(img)
+  });       
+
+}
+
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    setFavorites(data);
+  }, []);
+
+  useEffect(() => {
+    console.log(favorites);
+  }, [favorites]);
+
+  function handleFavorite(id) {
+    const newFavorites = favorites.map(item => {
+      return item.id === id ? { ...item, favorite: !item.favorite } : item;
+    });
+
+    setFavorites(newFavorites);
+  }
 
   return (
     <div className="App">
@@ -17,20 +58,34 @@ function App() {
           
         </div>
 
-
         <div className='divje'>
        
-          
           <RandomQuote />
          
-        
         </div>
-
-
       </header>
+      <ul>
+        {favorites.map((item, i) => (
+          <li key={i}>
+            {item.name}{" "}
+            <button
+              onClick={() => {
+                handleFavorite(item.id);
+              }}
+            >
+              {item.favorite === true ? "Remove" : "Add"}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <h1>Favorite list</h1>
+      <ul>
+        {favorites.map(item =>
+          item.favorite === true ? <li key={item.id}>{item.name}</li> : null
+        )}
+      </ul>
+
     </div>
   );
 }
-
-
-export default App;
---
+++ src/components/QuoteFavorite.js
@@ -0,0 +1,27 @@
+import React { useState, useEffect} from "react";
+import quote from "./quote";
+
+function QuoteFavorite () {
+    const fav = this.props.quotefav.map(item => (
+      <div className="listquote" key={item.id}>
+        <div className="quoteImage">
+          <img className="imgstyle" src={item.image} alt={item.name} />
+        </div>
+        <div className="quote">
+          {quote}
+            <button onClick={() => this.props.delete(item.id)}>
+              Delete from Favorite
+            </button>
+        </div>
+      </div>
+    ));
+    return (
+      <div>
+            <button onClick={() => QuoteFavorite}>
+              Add to Favorite
+            </button>
+      </div>
+    );
+  }
+
+export default QuoteFavorite;
